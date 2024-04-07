0e865f5207176c6dbaa4150e89ba02cceba25385
Duhun De Silva <sandaruduhun@gmail.com>
2024-04-07 19:23:41 +0530

    initial commit

--- src/Footer.js
+++ src/Footer.js
@@ -6,26 +6,35 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-section">
+        <div className="footer-section1">
           <h2>AT DIGITAL</h2>
           <p>Your goal is our target. Not anything in between. We use online marketing platforms and tools to achieve a single objective - your business results.</p>
         </div>
         <div className="footer-section">
-          <h3>Our Technologies</h3>
+          <h4>Our Technologies</h4>
           <ul>
-            {['ReactJS', 'Gatsby', 'NextJS', 'NodeJS'].map(tech => <li key={tech}>{tech}</li>)}
+          <li>ReactJS</li>
+          <li>Gatsby</li>
+          <li>NextJS</li>
+          <li>NodeJS</li>
+          
+
           </ul>
         </div>
         <div className="footer-section">
-          <h3>Our Services</h3>
+          <h4>Our Services</h4>
           <ul>
-            {['Social Media Marketing', 'Web & Mobile App Development', 'Data & Analytics'].map(service => <li key={service}>{service}</li>)}
+            <li>Social Media Marketing</li>
+            <li>Web & Mobile App</li>
+            <li>Development</li>
+            <li>Data & Analytics</li>
           </ul>
         </div>
-        <div className="footer-links">
+        
+      </div>
+      <div className="footer-links">
           Privacy Policy | Terms & Conditions
         </div>
-      </div>
     </footer>
   );
 }
